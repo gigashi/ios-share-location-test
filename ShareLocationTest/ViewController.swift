44c7ff64c7700d213fcefe61eafad9e6bc126ff5
Yoshiki Higashi <gigashiy@gmail.com>
2019-08-16 13:42:33 +0900

    [add] create project

---
+++ ShareLocationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShareLocationTest
+//
+//  Created by higashi_yo on 2019/08/16.
+//  Copyright © 2019 gigashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
